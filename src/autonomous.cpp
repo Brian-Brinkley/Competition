9608f6be24540a066906dad357b0a07e76918d65
Brian-Brinkley <briancbrinkley@gmail.com>
2025-12-11 09:52:53 -0600

    Co-authored-by: Logan  Kemp <LKemp@students.westtech.edu>

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -5,9 +5,18 @@ using namespace vex;
 void auton() {
     ErrorDrive errorDrive;
     if(finalChoice) {
-        
+
     }
     else {
-        
+        errorDrive.errorDist(50,1100);
+        Drivetrain.turnToHeading(90,degrees);
+        errorDrive.errorDist(50,275);
+        MotorGroup2.spin(reverse,80,percent);
+        wait(2,seconds);
+        MotorGroup2.stop(coast);
+        errorDrive.errorDist(-50,800);
+        MotorGroup2.spin(reverse,80,percent);
+        wait(2,seconds);
+        MotorGroup2.stop(coast);
     }
 }
