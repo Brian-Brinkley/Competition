5a51b5ac423d9eeaee0fd5937c74a443be23bad2
Brian-Brinkley <briancbrinkley@gmail.com>
2025-12-12 10:28:47 -0600

    Completed the right side routine for the autonomous funciton.

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -8,17 +8,19 @@ void auton() {
 
     }
     else {
+        Brain.Timer.reset();
         errorDrive.errorDist(50,1100);
         Drivetrain.turnToHeading(90,degrees);
-        errorDrive.errorDist(50,275);
-        MotorGroup2.spin(reverse,80,percent);
+        errorDrive.errorDist(50,285);
+        MotorGroup2.spin(reverse,70,percent);
         wait(2,seconds);
         MotorGroup2.stop(coast);
-        errorDrive.errorDist(-50,800);
-        Drivetrain.turnToHeading(85, degrees);
-        errorDrive.errorDist(20,30);
-        MotorGroup2.spin(reverse,80,percent);
+        errorDrive.errorDist(-50,50);
+        Drivetrain.turnToHeading(95, degrees);
+        errorDrive.errorDist(-70,800);
+        MotorGroup2.spin(reverse,100,percent);
         wait(2,seconds);
         MotorGroup2.stop(coast);
+        Brain.Screen.print(Brain.Timer.time(seconds));
     }
 }
