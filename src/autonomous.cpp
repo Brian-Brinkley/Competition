4854485d1c6b12a09ecaebfd8efb8302e3027a38
Brian-Brinkley <briancbrinkley@gmail.com>
2025-12-15 09:13:28 -0600

    Co-authored-by: Logan  Kemp <LKemp@students.westtech.edu>

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -13,14 +13,20 @@ void auton() {
         Drivetrain.turnToHeading(90,degrees);
         errorDrive.errorDist(50,285);
         MotorGroup2.spin(reverse,70,percent);
-        wait(2,seconds);
+        wait(2.2, seconds);
         MotorGroup2.stop(coast);
         errorDrive.errorDist(-50,50);
-        Drivetrain.turnToHeading(95, degrees);
+        Drivetrain.turnToHeading(92, degrees);
         errorDrive.errorDist(-70,800);
         MotorGroup2.spin(reverse,100,percent);
         wait(2,seconds);
         MotorGroup2.stop(coast);
+        Brain.Screen.print(Brain.Timer.time(seconds));
+        Brain.Screen.newLine();
+        errorDrive.errorDist(50,150);
+        PistonA.set(true);
+        wait(0.8,seconds);
+        errorDrive.errorDist(-50,80);
         Brain.Screen.print(Brain.Timer.time(seconds));
     }
 }
