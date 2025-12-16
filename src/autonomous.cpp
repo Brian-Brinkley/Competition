9eea48ad1bb5dca9716f4b28e0e50f8efb932d3f
Brian-Brinkley <briancbrinkley@gmail.com>
2025-12-16 09:40:29 -0600

    Added left side autonomous routine
    Co-authored-by: Logan  Kemp <LKemp@students.westtech.edu>

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -5,7 +5,26 @@ using namespace vex;
 void auton() {
     ErrorDrive errorDrive;
     if(finalChoice) {
-
+        Brain.Timer.reset();
+        errorDrive.errorDist(50,1100);
+        Drivetrain.turnToHeading(-90,degrees);
+        errorDrive.errorDist(50,285);
+        MotorGroup2.spin(reverse,70,percent);
+        wait(2.2, seconds);
+        MotorGroup2.stop(coast);
+        errorDrive.errorDist(-50,50);
+        Drivetrain.turnToHeading(-92, degrees);
+        errorDrive.errorDist(-70,800);
+        MotorGroup2.spin(reverse,100,percent);
+        wait(2,seconds);
+        MotorGroup2.stop(coast);
+        Brain.Screen.print(Brain.Timer.time(seconds));
+        Brain.Screen.newLine();
+        errorDrive.errorDist(50,150);
+        PistonA.set(true);
+        wait(0.8,seconds);
+        errorDrive.errorDist(-50,80);
+        Brain.Screen.print(Brain.Timer.time(seconds));
     }
     else {
         Brain.Timer.reset();
